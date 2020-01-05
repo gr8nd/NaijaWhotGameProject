c39a5b5ad0ffda0ef21c45e596561647b41a1f58
gr8ndi <swtworm@gmail.com>
2020-01-05 22:28:15 +0100

    Added the checkWinner and the rule methods, fixed some bugs as well.

--- src/Card.java
+++ src/Card.java
@@ -8,6 +8,22 @@ public class Card {
         this.face = face;
     }
 
+    public int getFace() {
+        return face;
+    }
+
+    public void setFace(int face) {
+        this.face = face;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
     public String toString() {
         return String.format("%d of %s", this.face, this.suit);
     }
--- src/WhotGame.java
+++ src/WhotGame.java
@@ -7,6 +7,13 @@ public class WhotGame {
     private ArrayList<Card> drawPile;
     private ArrayList<Card> computerCardPile;
     private ArrayList<Card> playerCardPile;
+    private int computerCount = 0;
+    private int playerCount = 0;
+    private boolean isHoldOn = false;
+    private boolean isSuspension = false;
+    private boolean isPickTwo = false;
+    private boolean isPickThree = false;
+    private boolean isWhot = false;
 
     public WhotGame() {
         whots = new NaijaWhots();
@@ -37,11 +44,64 @@ public class WhotGame {
     }
 
     public void rule(Card card) {
-
+        switch (card.getFace()) {
+            case 1:
+                isHoldOn = true;
+                break;
+            case 2:
+                isPickTwo = true;
+                break;
+            case 5:
+                isPickThree = true;
+                break;
+            case 8:
+                isSuspension = true;
+                break;
+            case 20:
+                isWhot = true;
+                break;
+        }
     }
 
-    public void checkWinner() {
+    public String checkWinner() {
+        if (drawPile.isEmpty() || computerCardPile.isEmpty() || playerCardPile.isEmpty()) {
+            if (computerCardPile.isEmpty()) {
+                isThereWinner = true;
+                return "*****Game Over*****\n\nYou lost!";
+            } else if (playerCardPile.isEmpty()) {
+                isThereWinner = true;
+                return "*****Game Over*****\n\nYou win!";
+            } else {
+                for (Card card : computerCardPile) {
+                    if (card.getSuit().equals("Star")) {
+                        computerCount += card.getFace() * 2;
+                    } else {
+                        computerCount += card.getFace();
+                    }
 
+                }
+                for (Card card : playerCardPile) {
+                    if (card.getSuit().equals("Star")) {
+                        playerCount += card.getFace() * 2;
+                    } else {
+                        playerCount += card.getFace();
+                    }
+
+                }
+                if (computerCount > playerCount) {
+                    isThereWinner = true;
+                    return "*****Game Over*****\n\nYou lost!";
+                } else if (computerCount < playerCount) {
+                    isThereWinner = true;
+                    return "*****Game Over*****\n\nYou win!";
+                } else {
+                    isThereWinner = true;
+                    return "*****Game Over*****\n\nA tie!";
+                }
+            }
+
+        }
+        return "No winner";
     }
 
     public boolean isThereWinner() {
@@ -74,5 +134,45 @@ public class WhotGame {
 
     public void setPlayerCardPile(ArrayList<Card> playerCardPile) {
         this.playerCardPile = playerCardPile;
+    }
+
+    public boolean isHoldOn() {
+        return isHoldOn;
+    }
+
+    public void setHoldOn(boolean holdOn) {
+        isHoldOn = holdOn;
+    }
+
+    public boolean isSuspension() {
+        return isSuspension;
+    }
+
+    public void setSuspension(boolean suspension) {
+        isSuspension = suspension;
+    }
+
+    public boolean isPickTwo() {
+        return isPickTwo;
+    }
+
+    public void setPickTwo(boolean pickTwo) {
+        isPickTwo = pickTwo;
+    }
+
+    public boolean isPickThree() {
+        return isPickThree;
+    }
+
+    public void setPickThree(boolean pickThree) {
+        isPickThree = pickThree;
+    }
+
+    public boolean isWhot() {
+        return isWhot;
+    }
+
+    public void setWhot(boolean whot) {
+        isWhot = whot;
     }
 }
