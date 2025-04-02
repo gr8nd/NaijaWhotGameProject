31a38ee4e949f5a8ad9fe10b4400641f1860ec3e
gr8nd <swtworm@gmail.com>
2025-04-02 21:33:23 +0100

    Fixed bugs in writing stats of the game as csv to file.

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -169,7 +169,7 @@ private void displayWinner()
      */
     private String buildCSV(List<Card> humanPlayedPile, List<Card> computerPlayedPile)
     {
-        String s = "id,humansuit,humanface,humantype,humanaction,humandefendcard,humanactiontaken" +
+        String s = "id,humansuit,humanface,humantype,humanaction,humandefendcard,humanactiontaken," +
                 "computersuit,computerface,computertype," +
                 "computeraction,computerdefendcard," +
                 "computeractiontaken,mode,forcewinner,humanwinner,computerwinner,tie";
